38f34fe9f00029b2e2ff15a01448c359ee58e2ad
123bhagesh <123bhagesh@gmail.com>
2022-05-17 23:07:48 +0530

    Day 1 Assignment 2 is complete

---
+++ ass2/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+import Logo from './components/Logo';
+import Links from './components/Links';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+
+        <Logo></Logo>
+        <Links></Links>
+        <Button></Button>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ass2/src/components/Links.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Links =()=>{
+
+    return<div id="linksDiv">
+        <h4>Service</h4>
+        <h4>Projects</h4>
+        <h4>About</h4>
+    </div>
+}
+
+export default Links;
